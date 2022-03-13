19942a4b26bd7536c9e3ab8a21b1d14c6dd48db4
praveen-1408 <100114320+praveen-1408@users.noreply.github.com>
2022-03-13 23:16:01 +0530

    Add files via upload

---
+++ soap-course-management/src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/soap/bean/Course.java
@@ -0,0 +1,5 @@
+package com.in28minutes.soap.webservices.soapcoursemanagement.soap.bean;
+
+public class Course {
+
+}
---
+++ soap-course-management/src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/soap/service/CourseDetailsService.java
@@ -0,0 +1,5 @@
+package com.in28minutes.soap.webservices.soapcoursemanagement.soap.service;
+
+public class CourseDetailsService {
+
+}
